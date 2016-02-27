3bfee389b1c8de22e3c3cf3d9d33d130d4f48eaf
Kanapriya <kanapriya@wso2.com>
2016-02-27 14:31:24 +0530

    Tag Added

--- src/test/java/org/wso2/carbon/connector/integration/test/freeagent/FreeagentConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/freeagent/FreeagentConnectorIntegrationTest.java
@@ -41,7 +41,7 @@ public class FreeagentConnectorIntegrationTest extends ConnectorIntegrationTestB
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
     
-        init("freeagent-connector-1.0.0");
+        init("freeagent-connector-1.0.1-SNAPSHOT");
         
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
